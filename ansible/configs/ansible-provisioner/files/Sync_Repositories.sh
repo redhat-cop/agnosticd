ee5c760f31ec0b4c2887c774e6749bb22f571623
sborenst <shachar.borenstein@gmail.com>
2017-04-19 16:33:07 +1000

    fixing cloudformation dependOn issue

--- ansible/configs/ansible-provisioner/files/Sync_Repositories.sh
+++ ansible/configs/ansible-provisioner/files/Sync_Repositories.sh
@@ -7,40 +7,33 @@
 #From you workstation host, run the following command to download/sync the repos down to the REPOSYNC_HOST
 # Your workstation needs to be able to authenticate to both the REPOSYNC_HOST and the TARGET_HOST
 
+# for
 export REPOVERSION="$1"
 export REPOMINORVERSION="$2"
+# for version 3.4 of ocp
+#export REPOMINORVERSION="3.4.1.12"
+#export REPOVERSION="3.4"
+
+# # # for version 3.5 of ocp
+# export REPOVERSION="3.5"
+# export REPOMINORVERSION="3.5.5.5"
+#
+
+
 export REPOLIST="rhel-7-fast-datapath-rpms rhel-7-server-extras-rpms rhel-7-server-optional-rpms rhel-7-server-ose-${REPOVERSION}-rpms rhel-7-server-rpms rhel-7-server-rh-common-rpms"
+#export REPOLIST="rhel-7-server-ose-${REPOVERSION}-rpms"
+
 export CLASS="ocp"
-export REPODIR="/var/www/html/repos/"
-export REPOPATH="${REPODIR}/${CLASS}/${REPOMINORVERSION}"
+export REPOBASE="/srv/repos/"
+export REPOPATH="${REPOBASE}/${CLASS}/${REPOMINORVERSION}"
 echo "REPOLIST is: $REPOLIST"
 echo "REPOVERSION is: ${REPOVERSION}"
 echo "CLASS is: $CLASS"
- #
- #
- # subscription-manager register --username=${RHNID} --password=${RHNPASS}
- # subscription-manager list --available
- # subscription-manager attach --pool=${RHNPOOL}
- # subscription-manager repos --disable="*"
- # subscription-manager repos \
- #    --enable="rhel-7-server-rpms" \
- #    --enable="rhel-7-server-extras-rpms" \
- #    --enable="rhel-7-server-ose-${REPOVERSION}-rpms" \
- #    --enable="rhel-7-server-optional-rpms" \
- #    --enable="rhel-7-server-rh-common-rpms" \
- #    --enable="rhel-7-fast-datapath-rpms"
- # yum install -y yum-utils createrepo tmux
- # yum clean all ; yum repolist
-
- # cd $REPOBASE
- # rm -rf $REPOBASE
- # mkdir -p $REPOBASE
- # cd $REPOBASE
 
 mkdir -p ${REPOPATH}
 
 cd ${REPOPATH}
-for repo_name in ${REPOLIST} ;
+time for repo_name in ${REPOLIST} ;
   do
     echo "Processing repo: ${repo_name}"
     echo "reposync -r ${repo_name}  -n -m -p ."
@@ -48,8 +41,29 @@ for repo_name in ${REPOLIST} ;
     cd ${repo_name}
     sudo createrepo .
     cd ..
-    #sudo tar -zcvf ${repo_name}.tar.gz ${repo_name}
     echo "Completed processing of: ${repo_name}"
   done
 
-ln -s ${REPOPATH} ${REPODIR}/${CLASS}/${REPOVERSION}
+ln -s ${REPOBASE}/${CLASS}/${REPOMINORVERSION} ${REPOBASE}/${CLASS}/${REPOVERSION}
+
+
+  #
+  #
+  # subscription-manager register --username=${RHNID} --password=${RHNPASS}
+  # subscription-manager list --available
+  # subscription-manager attach --pool=${RHNPOOL}
+  # subscription-manager repos --disable="*"
+  # subscription-manager repos \
+  #    --enable="rhel-7-server-rpms" \
+  #    --enable="rhel-7-server-extras-rpms" \
+  #    --enable="rhel-7-server-ose-${REPOVERSION}-rpms" \
+  #    --enable="rhel-7-server-optional-rpms" \
+  #    --enable="rhel-7-server-rh-common-rpms" \
+  #    --enable="rhel-7-fast-datapath-rpms"
+  # yum install -y yum-utils createrepo tmux
+  # yum clean all ; yum repolist
+
+  # cd $REPOBASE
+  # rm -rf $REPOBASE
+  # mkdir -p $REPOBASE
+  # cd $REPOBASE
