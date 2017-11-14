f14f963c4bf001813930eff9004ef070604d2af6
Guillaume Coré <gucore@redhat.com>
2017-11-14 17:43:02 +0100

    make fix IPA task async

--- ansible/roles/bastion-opentlc-ipa/files/fix_ipa.sh
+++ ansible/roles/bastion-opentlc-ipa/files/fix_ipa.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-
-/usr/bin/sss_ssh_authorizedkeys jenkins-sfo01
-if [ $? = 0 ]; then exit 0; fi
-
 systemctl restart sssd
-exit 1
+sleep 10
+
+for i in $(seq 20); do
+    /usr/bin/sss_ssh_authorizedkeys jenkins-sfo01
+    RET=$?
+    if [ $RET = 0 ]; then exit 0; fi
+
+    systemctl restart sssd
+    sleep 10
+done
+
+exit 2
