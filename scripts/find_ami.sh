c3d0323573db600010d2f8f7c8ebea45a8cd5e98
Guillaume Coré <gucore@redhat.com>
2017-12-15 23:54:51 +0100

    find_ami: search also Red Hat's official AMI

--- scripts/find_ami.sh
+++ scripts/find_ami.sh
@@ -3,5 +3,5 @@
 name=$1
 for region in $(aws ec2 describe-regions --query "Regions[].RegionName" --output text)
 do
-  echo "${region}: $(aws ec2 describe-images --owners amazon --filters Name=name,Values=${name} --query "reverse(sort_by(Images, &CreationDate))[0].ImageId" --output text --region $region)"
+  echo "${region}: $(aws ec2 describe-images --owners amazon 309956199498 --filters Name=name,Values=${name} --query "reverse(sort_by(Images, &CreationDate))[0].ImageId" --output text --region $region)"
 done
