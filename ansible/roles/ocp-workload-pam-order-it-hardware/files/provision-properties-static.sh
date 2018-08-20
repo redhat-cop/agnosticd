844766b6c7857f6f8d3dc2d05840dd992d699a91
Duncan Doyle <Duncan.Doyle@gmail.com>
2018-08-20 22:07:21 +0200

    Added RHPAM Authorhing deployment.

---
+++ ansible/roles/ocp-workload-pam-order-it-hardware/files/provision-properties-static.sh
@@ -0,0 +1,9 @@
+PRJ_NAME="rhpam7-mortgage"
+PRJ_DESCRIPTION="Red Hat Process Automation Manager 7 Mortgage Demo"
+
+BUSINESS_CENTRAL_REST_URL="$BC_URL/rest"
+SPACE="MySpace"
+DEFAULT_GROUPID="com.myspace"
+PROJECT_GIT="https://github.com/DuncanDoyle/IT_Orders.git"
+PROJECT_ID="IT_Orders"
+PROJECT_NAME="IT Orders"
