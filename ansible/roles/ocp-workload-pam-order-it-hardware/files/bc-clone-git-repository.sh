671741ff8dcef25e6088315b0ddd0e8ed722e766
Duncan Doyle <Duncan.Doyle@gmail.com>
2018-10-01 20:59:56 +0200

    Changed name to comply with naming conventions.

--- ansible/roles/ocp-workload-pam-order-it-hardware/files/bc-clone-git-repository.sh
+++ ansible/roles/ocp-workload-pam-order-it-hardware/files/bc-clone-git-repository.sh
@@ -11,7 +11,8 @@ launched=false
 echo "Trying to connect to spaces URL at: $BUSINESS_CENTRAL_REST_URL/spaces"
 until [ $count -gt $STARTUP_WAIT ]
 do
-  curl -u adminUser:test1234! --output /dev/null --silent --head --fail "$BUSINESS_CENTRAL_REST_URL/spaces"
+  #curl -u adminUser:test1234! --output /dev/null --silent --head --fail "$BUSINESS_CENTRAL_REST_URL/spaces"
+  curl -u $USERNAME:$PASSWORD --output /dev/null --silent --head --fail "$BUSINESS_CENTRAL_REST_URL/spaces"
   if [ $? -eq 0 ] ; then
     echo "PAM 7 Business Central REST API started."
     launched=true
@@ -31,12 +32,14 @@ else
   echo "PAM 7 Business Central started."
 fi
 
-CREATE_SPACE_JSON="{ \"name\":\"$SPACE\", \"description\":null, \"projects\":[], \"owner\":\"adminUser\", \"defaultGroupId\":\"$DEFAULT_GROUPID\"}"
+#CREATE_SPACE_JSON="{ \"name\":\"$SPACE\", \"description\":null, \"projects\":[], \"owner\":\"adminUser\", \"defaultGroupId\":\"$DEFAULT_GROUPID\"}"
+CREATE_SPACE_JSON="{ \"name\":\"$SPACE\", \"description\":null, \"projects\":[], \"owner\":\"$USERNAME\", \"defaultGroupId\":\"$DEFAULT_GROUPID\"}"
 #Create a space
-STATUSCODE=$(curl -H "Accept: application/json" -H "Content-Type: application/json" -f -X POST  -d "$CREATE_SPACE_JSON" -u "adminUser:test1234!" --silent --output /dev/null --write-out "%{http_code}" "$BUSINESS_CENTRAL_REST_URL/spaces")
+#STATUSCODE=$(curl -H "Accept: application/json" -H "Content-Type: application/json" -f -X POST  -d "$CREATE_SPACE_JSON" -u "adminUser:test1234!" --silent --output /dev/null --write-out "%{http_code}" "$BUSINESS_CENTRAL_REST_URL/spaces")
+STATUSCODE=$(curl -H "Accept: application/json" -H "Content-Type: application/json" -f -X POST  -d "$CREATE_SPACE_JSON" -u "$USERNAME:$PASSWORD" --silent --output /dev/null --write-out "%{http_code}" "$BUSINESS_CENTRAL_REST_URL/spaces")
 
 if [ $STATUSCODE -ne 202 ] ; then
-    echo "Error creating new Space. Exiting"
+    echo "Error creating new Space. HTTP Status Code: $STATUSCODE. Exiting"
     exit 1
 else
     echo "Creating new Space."
@@ -48,7 +51,8 @@ count=0
 created=false
 until [ $count -gt $STARTUP_WAIT ]
 do
-  curl -u adminUser:test1234! --output /dev/null --silent --head --fail "$BUSINESS_CENTRAL_REST_URL/spaces/$SPACE"
+  #curl -u adminUser:test1234! --output /dev/null --silent --head --fail "$BUSINESS_CENTRAL_REST_URL/spaces/$SPACE"
+  curl -u $USERNAME:$PASSWORD --output /dev/null --silent --head --fail "$BUSINESS_CENTRAL_REST_URL/spaces/$SPACE"
   if [ $? -eq 0 ] ; then
     echo "\nSpace created."
     created=true
@@ -64,12 +68,14 @@ sleep 3
 
 # Check if the project is already present. If it is, we simply skip cloning
 #Create a space
-curl -u adminUser:test1234! --output /dev/null --silent --fail "$BUSINESS_CENTRAL_REST_URL/spaces/$SPACE/projects/$PROJECT_ID"
+#curl -u adminUser:test1234! --output /dev/null --silent --fail "$BUSINESS_CENTRAL_REST_URL/spaces/$SPACE/projects/$PROJECT_ID"
+curl -u $USERNAME:$PASSWORD --output /dev/null --silent --fail "$BUSINESS_CENTRAL_REST_URL/spaces/$SPACE/projects/$PROJECT_ID"
 if [ $? -ne 0 ] ; then
    echo "Cloning project.."
    # And clone the project into that space
    CLONE_GIT_JSON="{\"name\":\"$PROJECT_ID\", \"gitURL\":\"$PROJECT_GIT\"}"
-   STATUSCODE=$(curl -H "Accept: application/json" -H "Content-Type: application/json" -f -X POST  -d "$CLONE_GIT_JSON" -u "adminUser:test1234!" --silent --output /dev/null --write-out "%{http_code}" "$BUSINESS_CENTRAL_REST_URL/spaces/$SPACE/git/clone")
+   #STATUSCODE=$(curl -H "Accept: application/json" -H "Content-Type: application/json" -f -X POST  -d "$CLONE_GIT_JSON" -u "adminUser:test1234!" --silent --output /dev/null --write-out "%{http_code}" "$BUSINESS_CENTRAL_REST_URL/spaces/$SPACE/git/clone")
+   STATUSCODE=$(curl -H "Accept: application/json" -H "Content-Type: application/json" -f -X POST  -d "$CLONE_GIT_JSON" -u "$USERNAME:$PASSWORD" --silent --output /dev/null --write-out "%{http_code}" "$BUSINESS_CENTRAL_REST_URL/spaces/$SPACE/git/clone")
    if [ $STATUSCODE -ne 202 ] ; then
       echo "Error cloning Demo Git repository. Exiting"
       exit 1
