73a0a4d370672418345c6183663c3212b6fa86c8
Duncan Doyle <Duncan.Doyle@gmail.com>
2018-10-16 17:50:44 +0200

    Finished deployment scripts for DM7 QLB demo to a point where the containers deploy on a local OCP. They're still giving issues on RHPDS, as containers don't spin up because they don't request a CPU per container that is above the minimum.

--- ansible/roles/ocp-workload-dm7-qlb-demo/files/dc-clone-git-repository.sh
+++ ansible/roles/ocp-workload-dm7-qlb-demo/files/dc-clone-git-repository.sh
@@ -1,23 +1,19 @@
 #!/bin/sh
 
-DECISION_CENTRAL_REST_URL="http://rhdm7-qlb-rhdmcentr-dm7-qlb-duncandoyle.192.168.64.16.nip.io/rest"
-#DECISION_CENTRAL_REST_URL="http://rhdm7-qlb-rhdmcentr:8080/rest"
-SPACE="MySpace"
-DEFAULT_GROUPID="com.myspace"
-PROJECT_GIT="https://github.com/jbossdemocentral/rhdm7-loan-demo-repo.git"
-PROJECT_ID="rhpam7-mortgage-demo-repo"
-PROJECT_NAME="Mortgage_Demo"
-STARTUP_WAIT=60
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. $SCRIPT_DIR/provision-properties-static.sh
+STARTUP_WAIT=180
 
-#First check if the DM 7 Decision Central REST API is available. We'll wait for 60 seconds
+#First check if the PAM 7 Decision Central REST API is available. We'll wait for 60 seconds
 echo "Locating Decision Central REST API."
 count=0
 launched=false
+echo "Trying to connect to spaces URL at: $Decision_CENTRAL_REST_URL/spaces"
 until [ $count -gt $STARTUP_WAIT ]
 do
-  curl -u dmAdmin:redhatdm1! --output /dev/null --silent --head --fail "$DECISION_CENTRAL_REST_URL/spaces"
+  curl -u $USERNAME:$PASSWORD --output /dev/null --silent --head --fail "$DECISION_CENTRAL_REST_URL/spaces"
   if [ $? -eq 0 ] ; then
-    echo "DM 7 Decision Central REST API started."
+    echo "PAM 7 Decision Central REST API started."
     launched=true
     break
   fi
@@ -29,18 +25,19 @@ done
 #Check that the platform has started, otherwise exit.
 if [ $launched = "false" ]
 then
-  echo "DM 7 Decision Central did not start correctly. Exiting."
+  echo "PAM 7 Decision Central did not start correctly. Exiting."
   exit 1
 else
-  echo "DM 7 Decision Central started."
+  echo "PAM 7 Decision Central started."
 fi
 
-CREATE_SPACE_JSON="{ \"name\":\"$SPACE\", \"description\":null, \"projects\":[], \"owner\":\"dmAdmin\", \"defaultGroupId\":\"$DEFAULT_GROUPID\"}"
+#CREATE_SPACE_JSON="{ \"name\":\"$SPACE\", \"description\":null, \"projects\":[], \"owner\":\"adminUser\", \"defaultGroupId\":\"$DEFAULT_GROUPID\"}"
+CREATE_SPACE_JSON="{ \"name\":\"$SPACE\", \"description\":null, \"projects\":[], \"owner\":\"$USERNAME\", \"defaultGroupId\":\"$DEFAULT_GROUPID\"}"
 #Create a space
-STATUSCODE=$(curl -H "Accept: application/json" -H "Content-Type: application/json" -f -X POST  -d "$CREATE_SPACE_JSON" -u "dmAdmin:redhatdm1!" --silent --output /dev/null --write-out "%{http_code}" "$DECISION_CENTRAL_REST_URL/spaces")
+STATUSCODE=$(curl -H "Accept: application/json" -H "Content-Type: application/json" -f -X POST  -d "$CREATE_SPACE_JSON" -u "$USERNAME:$PASSWORD" --silent --output /dev/null --write-out "%{http_code}" "$DECISION_CENTRAL_REST_URL/spaces")
 
 if [ $STATUSCODE -ne 202 ] ; then
-    echo "Error creating new Space. Exiting"
+    echo "Error creating new Space. HTTP Status Code: $STATUSCODE. Exiting"
     exit 1
 else
     echo "Creating new Space."
@@ -52,7 +49,8 @@ count=0
 created=false
 until [ $count -gt $STARTUP_WAIT ]
 do
-  curl -u dmAdmin:redhatdm1! --output /dev/null --silent --head --fail "$DECISION_CENTRAL_REST_URL/spaces/$SPACE"
+  #curl -u adminUser:test1234! --output /dev/null --silent --head --fail "$Decision_CENTRAL_REST_URL/spaces/$SPACE"
+  curl -u $USERNAME:$PASSWORD --output /dev/null --silent --head --fail "$DECISION_CENTRAL_REST_URL/spaces/$SPACE"
   if [ $? -eq 0 ] ; then
     echo "\nSpace created."
     created=true
@@ -68,12 +66,13 @@ sleep 3
 
 # Check if the project is already present. If it is, we simply skip cloning
 #Create a space
-curl -u dmAdmin:redhatdm1! --output /dev/null --silent --fail "$DECISION_CENTRAL_REST_URL/spaces/$SPACE/projects/$PROJECT_NAME"
+curl -u $USERNAME:$PASSWORD --output /dev/null --silent --fail "$DECISION_CENTRAL_REST_URL/spaces/$SPACE/projects/$PROJECT_ID"
 if [ $? -ne 0 ] ; then
    echo "Cloning project.."
    # And clone the project into that space
    CLONE_GIT_JSON="{\"name\":\"$PROJECT_ID\", \"gitURL\":\"$PROJECT_GIT\"}"
-   STATUSCODE=$(curl -H "Accept: application/json" -H "Content-Type: application/json" -f -X POST  -d "$CLONE_GIT_JSON" -u "dmAdmin:redhatdm1!" --silent --output /dev/null --write-out "%{http_code}" "$DECISION_CENTRAL_REST_URL/spaces/$SPACE/git/clone")
+   #STATUSCODE=$(curl -H "Accept: application/json" -H "Content-Type: application/json" -f -X POST  -d "$CLONE_GIT_JSON" -u "adminUser:test1234!" --silent --output /dev/null --write-out "%{http_code}" "$Decision_CENTRAL_REST_URL/spaces/$SPACE/git/clone")
+   STATUSCODE=$(curl -H "Accept: application/json" -H "Content-Type: application/json" -f -X POST  -d "$CLONE_GIT_JSON" -u "$USERNAME:$PASSWORD" --silent --output /dev/null --write-out "%{http_code}" "$DECISION_CENTRAL_REST_URL/spaces/$SPACE/git/clone")
    if [ $STATUSCODE -ne 202 ] ; then
       echo "Error cloning Demo Git repository. Exiting"
       exit 1
