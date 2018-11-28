6c89f22ff179155417aa5d458616b6fb6909ba8e
sborenst <shachar.borenstein@gmail.com>
2018-11-28 11:55:17 +1100

    tested ocp-clientvm and three-tier-app

--- ansible/roles/ocp-workload-fuse-on-ocp/ilt_provision.sh
+++ ansible/roles/ocp-workload-fuse-on-ocp/ilt_provision.sh
@@ -75,7 +75,7 @@ function executeAnsible() {
     ansible-playbook -i ${TARGET_HOST}, ./configs/ocp-workloads/ocp-workload.yml \
                  -e"ansible_ssh_private_key_file=~/.ssh/${SSH_PRIVATE_KEY}" \
                  -e"ansible_ssh_user=${SSH_USERNAME}" \
-                    -e"ANSIBLE_REPO_PATH | default('.')=`pwd`" \
+
                     -e"ocp_username=${OCP_USERNAME}" \
                     -e"ocp_workload=${WORKLOAD}" \
                     -e"guid=${GUID}" \
