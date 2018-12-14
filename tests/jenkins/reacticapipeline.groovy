f0333a0e90b234ce886f61de1df1d68e67f48180
Chandrabose Bomman <bosebc@github.com>
2018-12-14 17:08:15 +0530

    Added regex to catch the error. 'by Bose B Chandra'

--- tests/jenkins/reacticapipeline.groovy
+++ tests/jenkins/reacticapipeline.groovy
@@ -159,10 +159,24 @@ pipeline {
                         """
                     ).trim()
 
+		    echo email	
+                    try {
+                        def m = email =~ /Openshift Master Console: (https:\/\/master\.[^ ]+)/
+                        openshift_location = m[0][1]
+                        echo "openshift_location = '${openshift_location}'"
 
-                    def m = email =~ /<pre>. *ssh -i [^ ]+ *([^ <]+?) *<\/pre>/
-                    ssh_location = m[0][1]
-                    echo "ssh_location = '${ssh_location}'"
+                        m = email =~ /Web App URL: (https:\/\/[^ \n]+)/
+                        webapp_location = m[0][1]
+                        echo "webapp_location = '${openshift_location}'"
+
+                        m = email =~ /Cluster Admin User: ([^ \n]+ \/ [^ \n]+)/
+                        echo "Custer Admin User: ${m[0][1]}"
+                    } catch(Exception ex) {
+                        echo "Could not parse email:"
+                        echo email
+                        echo ex.toString()
+                        throw ex
+                    }
                 }
             }
         }
