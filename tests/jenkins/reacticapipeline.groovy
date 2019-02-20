8bb5707b0e5c44ad3e2c930538637cbd54e5ffdd
Chandrabose Bomman <bosebc@github.com>
2019-02-20 17:03:04 +0530

    Added ssh code to admin host to grap OKTODELETE from the build log.

--- tests/jenkins/reacticapipeline.groovy
+++ tests/jenkins/reacticapipeline.groovy
@@ -12,10 +12,10 @@ def notification_email = 'cbomman@redhat.com'
 def rocketchat_hook = '5d28935e-f7ca-4b11-8b8e-d7a7161a013a'
 
 // SSH key
-//def ssh_creds = '15e1788b-ed3c-4b18-8115-574045f32ce4'
+def ssh_creds = '15e1788b-ed3c-4b18-8115-574045f32ce4'
 
 // Admin host ssh location is in a credential too
-//def ssh_admin_host = 'admin-host-na'
+def ssh_admin_host = 'admin-host-na'
 
 // state variables
 def guid=''
@@ -292,6 +292,27 @@ pipeline {
                     from: credentials.split(':')[0]
               )
             }
+
+	    withCredentials([
+                string(credentialsId: ssh_admin_host, variable: 'ssh_admin'),
+                sshUserPrivateKey(
+                    credentialsId: ssh_creds,
+                    keyFileVariable: 'ssh_key',
+                    usernameVariable: 'ssh_username')
+            ]) {
+                script {
+                  // I haven't tested this, but it gives you the idea
+                  // Be careful with commands executed on admin host, make sure it's 'read-only' commands
+                  def retstring = sh("""
+                    ssh -o StrictHostKeyChecking=no -i ${ssh_key} ${ssh_admin} \
+                    "find deployer_logs -name '*${guid}*log' | xargs -n1 grep OKTODELETE"
+                    exit 0
+                  """.trim()
+                  )
+                  assert retstring.contains("OKTODELETE")
+                }
+            }
+
             withCredentials([string(credentialsId: rocketchat_hook, variable: 'HOOK_URL')]) {
                 sh(
                     """
