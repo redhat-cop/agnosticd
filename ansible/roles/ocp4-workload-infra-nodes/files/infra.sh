1ad15d992b274aa28a3e6d3bb68a0995a432e8a3
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2019-04-05 12:48:10 +0200

    Fixing logging and infra roles

--- ansible/roles/ocp4-workload-infra-nodes/files/infra.sh
+++ ansible/roles/ocp4-workload-infra-nodes/files/infra.sh
@@ -7,7 +7,7 @@ oc get machineset -n openshift-machine-api -o json\
   | jq '.spec.selector.matchLabels."machine.openshift.io/cluster-api-machineset"=env["NAME"]'\
   | jq '.spec.template.metadata.labels."machine.openshift.io/cluster-api-machineset"=env["NAME"]'\
   | jq '.spec.template.spec.metadata.labels."node-role.kubernetes.io/infra"=""'\
-  | jq '.spec.template.spec.providerSpec.value.instanceType="m5.4xlarge"'\
+  | jq '.spec.template.spec.providerSpec.value.instanceType="m4.4xlarge"'\
   | jq 'del (.metadata.annotations)'\
   | jq '.metadata.labels += {"machineset":"infra"}'\
   | jq '.spec.replicas=3'\
