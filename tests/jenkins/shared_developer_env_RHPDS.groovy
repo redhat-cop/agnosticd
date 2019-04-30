ceb1ccede5ba76c0399b758632f41baefdc6422e
Guillaume Coré <gucore@redhat.com>
2019-04-30 08:38:23 +0200

    OCP3 Shared Access pipeline: Update catalog item name
    This commit, if applied, will update the Catalog Item name jenkins will order in
    CloudForms.
    Name was changed in Cloudforms: the pipeline is currently broken.

--- tests/jenkins/shared_developer_env_RHPDS.groovy
+++ tests/jenkins/shared_developer_env_RHPDS.groovy
@@ -48,7 +48,7 @@ pipeline {
                         script: '''
                           ./opentlc/order_svc_guid.sh \
                           -c "OpenShift Demos" \
-                          -i "OpenShift 3.11 Shared Environment (TEST)" \
+                          -i "OpenShift 3.11 Shared Environment" \
                           -d "check=t,quotacheck=t,runtime=8,expiration=7,nodes=1,region=global_gpte"
                         '''
                     ).trim()
