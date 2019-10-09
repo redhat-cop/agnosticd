b849952cb8742546838489de8790788f5beb8493
Daniel Oh <doh@redhat.com>
2019-10-09 18:02:28 -0400

    Checking if che is up for prewarm workspaces (#751)
    * added 3.11.51 host template
    
    * Add Hands-On Lab for Advanced Cloud-Native App Development for RHTE 2019
    
    * update workload
    
    * update workload
    
    * fix deployment issue
    
    * fix deployment issue
    
    * fix deployment
    
    * update num_users
    
    * delete dummy file
    
    * update ocp_username
    
    * update cdw pvc setting
    
    * fix the Import realm
    
    * update m4
    
    * update cdw
    
    * update workload to add kafka, knative
    
    * delete Untitled-1
    
    * add load-balancer-attributes
    
    * add prewarm cdw
    
    * add prewarm cdw
    
    * add prewarm cdw
    
    * remove node scaling
    
    * Implement  Post Workload deployment tasks
    
    * make sure if che is up
    
    * make sure if che is up
    
    * make sure if che is up

--- ansible/roles/ocp4-workload-ccnrd/files/preparelab_ccn.sh
+++ ansible/roles/ocp4-workload-ccnrd/files/preparelab_ccn.sh
@@ -632,7 +632,9 @@ done
 # workaround for PVC problem
 wget https://raw.githubusercontent.com/RedHat-Middleware-Workshops/cloud-native-workshop-v2-infra/ocp-4.1/files/cm-custom-codeready.yaml
 oc apply -f cm-custom-codeready.yaml -n labs-infra
+
 oc scale -n labs-infra deployment/codeready --replicas=0
+sleep 10
 oc scale -n labs-infra deployment/codeready --replicas=1
 
 # Wait for che to be back up
@@ -708,6 +710,17 @@ curl -X POST --header 'Content-Type: application/json' --header 'Accept: applica
 # import stack image
 oc create -n openshift -f https://raw.githubusercontent.com/RedHat-Middleware-Workshops/cloud-native-workshop-v2-infra/ocp-4.1/files/stack.imagestream.yaml
 oc import-image --all quarkus-stack -n openshift
+
+# Checking if che is up
+echo "Checking if che is up..."
+while [ 1 ]; do
+  STAT=$(curl -s -w '%{http_code}' -o /dev/null http://codeready-labs-infra.$HOSTNAME_SUFFIX/dashboard/)
+  if [ "$STAT" = 200 ] ; then
+    break
+  fi
+  echo -n .
+  sleep 10
+done
 
 # Pre-create workspaces for users
 for i in $(eval echo "{0..$USERCOUNT}") ; do
