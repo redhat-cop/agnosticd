4a24218f8a3d667114ff626e7b085cc16b2abb79
Jim Rigsbee <jrigsbee@redhat.com>
2019-11-13 15:06:24 -0600

    OCP4 Workshop: fixed bugs with deploying Lets Encrypt certs upon renewal

--- ansible/roles/ocp4-workload-enable-lets-encrypt-certificates/files/deploy_certs.sh
+++ ansible/roles/ocp4-workload-enable-lets-encrypt-certificates/files/deploy_certs.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+pushd ~/certbot/config/renewal-hooks/deploy
 ansible-playbook ./deploy_certs.yml
+popd
--- ansible/roles/ocp4-workload-idm/files/deploy_certs.sh
+++ ansible/roles/ocp4-workload-idm/files/deploy_certs.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+pushd ~/idm/certbot/config/renewal-hooks/deploy
 ansible-playbook ./deploy_certs.yml \
   -e "_certbot_domain={{ idm_dns_name }}" \
-  -e "idm_dm_password={{ idm_dm_password }}"
+  -e 'idm_dm_password={{ idm_dm_password }}'
+popd
