1e1c1a19e3b53ff3a769d88f9d2ffed1984ff86a
ecwpz91 <ecwpz91@users.noreply.github.com>
2020-02-06 12:28:53 -0500

    Added module check to delete jenkins, and formatted script

--- ansible/roles/ocp-workload-ccnrd/files/resetlab_ccn.sh
+++ ansible/roles/ocp-workload-ccnrd/files/resetlab_ccn.sh
@@ -4,54 +4,65 @@
 #
 MYDIR="$( cd "$(dirname "$0")" ; pwd -P )"
 function usage() {
-    echo "usage: $(basename $0)"
+ echo "usage: $(basename $0)"
 }
+
 # Defaults
 USERCOUNT=3
+MODULE_TYPE=m1
 
 POSITIONAL=()
 while [[ $# -gt 0 ]]
 do
-key="$1"
+ key="$1"
 
-case $key in
-    -c|--count)
-    USERCOUNT="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    *)    # unknown option
-    echo "Unknown option: $key"
-    usage
-    exit 1
-    ;;
-esac
+ case $key in
+  -c|--count)
+   USERCOUNT="$2"
+   shift # past argument
+   shift # past value
+   ;;
+  -m|--module-type)
+   MODULE_TYPE="$2"
+   shift # past argument
+   shift # past value
+   ;;
+  *)    # unknown option
+   echo "Unknown option: $key"
+   usage
+   exit 1
+   ;;
+ esac
 done
 
 if [ ! "$(oc get clusterrolebindings)" ] ; then
-  echo "not cluster-admin"
-  exit 1
+ echo "not cluster-admin"
+ exit 1
 fi
 
 # Remove view role of default namespace to all userXX
 for i in $(eval echo "{0..$USERCOUNT}") ; do
-  oc adm policy remove-role-from-user view user$i -n default
-  echo -n .
-  sleep 2
+ oc adm policy remove-role-from-user view user$i -n default
+ echo -n .
+ sleep 2
 done
 
 oc delete project labs-infra
 oc delete template coolstore-monolith-binary-build coolstore-monolith-pipeline-build ccn-sso72 -n openshift
 oc delete CatalogSourceConfig/installed-redhat-che -n openshift-marketplace
 
+if [ -z "${MODULE_TYPE##*m2*}" ] ; then
+ oc delete project jenkins
+fi
+
 # delete user projects
 for proj in $(oc get projects -o name | grep 'user*' | cut -d/ -f2) ; do
-  oc delete project $proj
+ oc delete project $proj
 done
 
 # delete dummy projects
 for proj in $(oc get projects -o name | grep 'dummy*' | cut -d/ -f2) ; do
-  oc delete project $proj
+ oc delete project $proj
 done
 
 # delete json files
