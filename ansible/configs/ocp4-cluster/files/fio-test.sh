da95aea8641d5ccd762cfaf3d9d2c127e6b0bb53
stencell <nate@redhat.com>
2020-03-30 18:32:23 -0700

    fix bash seq

--- ansible/configs/ocp4-cluster/files/fio-test.sh
+++ ansible/configs/ocp4-cluster/files/fio-test.sh
@@ -8,7 +8,7 @@ echo "running tests"
 
 mkdir -p /var/lib/etcd/fio
 # GUID=$(echo $HOSTNAME |cut -d \- -f 1)
-for i in {1..$test_runs}
+for i in $(seq $test_runs)
 do
   echo "running test $i"
   result=$(fio --rw=write --ioengine=sync --fdatasync=1 --directory=/var/lib/etcd/fio --size=22m --bs=2300 --name=mytest --output-format=json+ | jq '.jobs[].sync.lat_ns.percentile."99.000000"')
