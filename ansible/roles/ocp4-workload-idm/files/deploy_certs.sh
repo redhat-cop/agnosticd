46055aafbd9bd01f08158a013e8f1c0ac27854c4
Guillaume Coré <gucore@redhat.com>
2020-07-16 15:47:11 +0200

    Fix LetsEncrypt renewal hook (#2139)
    This commit, if applied, fixes the following error:
    
    ```
    Running post-hook command: /home/ec2-user/certbot/renewal-hooks/deploy/deploy_certs.sh
    post-hook command "/home/ec2-user/certbot/renewal-hooks/deploy/deploy_certs.sh" returned error code 1
    Error output from post-hook command deploy_certs.sh:
    ERROR! the playbook: ./deploy_certs.yml could not be found
    ```

--- ansible/roles/ocp4-workload-idm/files/deploy_certs.sh
+++ ansible/roles/ocp4-workload-idm/files/deploy_certs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd ${HOME}/certbot/renewal-hooks/deploy/
 ansible-playbook ./deploy_certs.yml \
   -e "_certbot_domain={{ idm_dns_name }}" \
   -e "idm_dm_password={{ idm_dm_password }}"
