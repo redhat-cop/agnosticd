0e657016dc43eca5ebe66094e1caf2cac6ce01c6
Dibyendu Jana <djana@redhat.com>
2020-08-22 00:24:39 +0530

    Increase timeout for email checking

--- tests/jenkins/sap-end-to-end-automation.groovy
+++ tests/jenkins/sap-end-to-end-automation.groovy
@@ -148,7 +148,7 @@ pipeline {
                           ./tests/jenkins/downstream/poll_email.py \
                           --server '${imap_server}' \
                           --guid ${guid} \
-                          --timeout 30 \
+                          --timeout 150 \
                           --filter 'has completed'
                         """
                     ).trim()
