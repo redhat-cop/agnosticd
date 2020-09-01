a55abb93adfd2b6a73a2a50effb72fd58551c9a6
Dibyendu Jana <djana@redhat.com>
2020-09-01 07:19:45 +0530

    Fixing CI dialog input

--- tests/jenkins/sap-side-by-side-integration.groovy
+++ tests/jenkins/sap-side-by-side-integration.groovy
@@ -90,7 +90,7 @@ pipeline {
                         'status=t',
                         'check=t',
                         'check2=t',
-                        'salesforce=gptejen',
+                        'notes=devops_automation_jenkins',
                         'expiration=2',
                         'runtime=10',
                         'quotacheck=t',
