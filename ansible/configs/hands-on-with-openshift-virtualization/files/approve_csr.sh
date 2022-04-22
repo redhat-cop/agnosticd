581c07a5e321772b5872395bcfcfc3589886d6eb
agonzalezrh <alberto.gonzalez@redhat.com>
2022-04-22 09:12:08 +0200

    Update approve_csr.sh

--- ansible/configs/hands-on-with-openshift-virtualization/files/approve_csr.sh
+++ ansible/configs/hands-on-with-openshift-virtualization/files/approve_csr.sh
@@ -1,10 +1,10 @@
 #!/bin/sh 
-EXPECTED_NODES=$1
 export KUBECONFIG=/home/lab-user/install/auth/kubeconfig
-NOTREADY=$(oc get nodes |grep -c NotReady)
+sleep 30
+NOTREADY=$(/usr/local/bin/oc get nodes |grep -c NotReady)
 
 while [ $NOTREADY -gt 0 ]; do
-        oc get csr|grep Pending|awk '{print $1}'|xargs -i oc adm certificate approve {}
+        /usr/local/bin/oc get csr|grep Pending|awk '{print $1}'|xargs -i oc adm certificate approve {}
         sleep 30
-        NOTREADY=$(oc get nodes |grep -c NotReady)
+        NOTREADY=$(/usr/local/bin/oc get nodes |grep -c NotReady)
 done
