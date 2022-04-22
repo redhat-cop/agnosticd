cd56ad6fabb4a86a1190407b131f8b8660a33cf6
agonzalezrh <alberto.gonzalez@redhat.com>
2022-04-22 12:34:32 +0100

    Update osp_cloud_template_master.j2

--- ansible/configs/hands-on-with-openshift-virtualization/files/approve_csr.sh
+++ ansible/configs/hands-on-with-openshift-virtualization/files/approve_csr.sh
@@ -1,10 +1,9 @@
 #!/bin/sh 
-EXPECTED_NODES=$1
 export KUBECONFIG=/home/lab-user/install/auth/kubeconfig
-NOTREADY=$(oc get nodes |grep -c NotReady)
+NOTREADY=$(/usr/local/bin/oc get nodes |grep -c NotReady)
 
 while [ $NOTREADY -gt 0 ]; do
-        oc get csr|grep Pending|awk '{print $1}'|xargs -i oc adm certificate approve {}
-        sleep 30
-        NOTREADY=$(oc get nodes |grep -c NotReady)
+        /usr/local/bin/oc get csr|grep Pending|awk '{print $1}'|xargs -i /usr/local/bin/oc adm certificate approve {}
+        NOTREADY=$(/usr/local/bin/oc get nodes |grep -c NotReady)
 done
+exit 0
