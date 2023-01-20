ee190dc8732a0cb2f791246319895c3b38689736
Alberto Gonzalez <josegonz@redhat.com>
2023-01-20 21:50:49 +0100

    Adding remaining tasks for RHTE

---
+++ ansible/configs/migrating-to-ocpvirt/files/configure_ssl.sh
@@ -0,0 +1,18 @@
+cd /root/
+oc create configmap custom-ca \
+     --from-file=ca-bundle.crt=chain1.pem \
+     -n openshift-config
+
+oc patch proxy/cluster \
+     --type=merge \
+     --patch='{"spec":{"trustedCA":{"name":"custom-ca"}}}'
+
+oc create secret tls letsencrypt \
+     --cert=cert1.pem \
+     --key=privkey1.pem \
+     -n openshift-ingress
+
+oc patch ingresscontroller.operator default \
+     --type=merge -p \
+     '{"spec":{"defaultCertificate": {"name": "letsencrypt"}}}' \
+     -n openshift-ingress-operator
