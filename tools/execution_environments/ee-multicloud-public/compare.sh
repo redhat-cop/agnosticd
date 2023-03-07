d8be6d5a088b6f7c2a9b60b4464902df63ffc705
Guillaume Coré <gucore@redhat.com>
2023-03-07 09:34:32 +0100

    test podman command

--- tools/execution_environments/ee-multicloud-public/compare.sh
+++ tools/execution_environments/ee-multicloud-public/compare.sh
@@ -5,9 +5,18 @@
 v1=$1
 v2=$2
 
+if [ -z "${v1}" ] || [ -z "${v2}" ]; then
+	echo "$0 [v1] [v2]"
+	echo "ex:  $0 quay.io/agnosticd/ee-multicloud:v0.0.9 quay.io/agnosticd/ee-multicloud:latest"
+	exit 1
+fi
+
 diff -u \
     <(podman run  -v ./:/tmp/a \
     --entrypoint=/tmp/a/ee-report.sh $v1) \
     <(podman run  -v ./:/tmp/a \
     --entrypoint=/tmp/a/ee-report.sh $v2) \
-    | gh gist create --public -f "ee-report.diff" -d "ee-report ee-multicloud ${v1} to ${v2}"
+    | gh gist create --public -f "ee-report.diff" -d "ee-report ${v1} to ${v2}"
+
+podman run -v ./:/tmp/a --entrypoint=/tmp/a/ee-report.sh $v2 \
+	| gh gist create --public -f "ee-report.txt" -d "ee-report ${v2}"
