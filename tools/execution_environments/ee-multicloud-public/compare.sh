076e8646a2f276d06f8ed776e76484e96b62d428
Guillaume Coré <gucore@redhat.com>
2023-03-07 10:24:29 +0100

    Add changelog entry

--- tools/execution_environments/ee-multicloud-public/compare.sh
+++ tools/execution_environments/ee-multicloud-public/compare.sh
@@ -5,6 +5,12 @@
 v1=$1
 v2=$2
 
+if [ -z "${v1}" ] || [ -z "${v2}" ]; then
+	echo "$0 [v1] [v2]"
+	echo "ex:  $0 quay.io/agnosticd/ee-multicloud:v0.0.9 quay.io/agnosticd/ee-multicloud:latest"
+	exit 1
+fi
+
 diff -u \
     <(podman run  -v ./:/tmp/a \
     --entrypoint=/tmp/a/ee-report.sh $v1) \
