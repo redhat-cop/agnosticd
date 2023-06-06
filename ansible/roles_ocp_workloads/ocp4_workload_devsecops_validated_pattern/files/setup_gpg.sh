10688c4c1d09fc6bb7b6593ec904574f863cb09a
treddy08 <treddy@redhat.com>
2023-06-07 09:24:29 +1000

    update

--- ansible/roles_ocp_workloads/ocp4_workload_devsecops_validated_pattern/files/setup_gpg.sh
+++ ansible/roles_ocp_workloads/ocp4_workload_devsecops_validated_pattern/files/setup_gpg.sh
@@ -25,8 +25,4 @@ gpg --pinentry-mode loopback \$@
 EOF
 chmod +x ~/.gpg
 gpg-connect-agent reloadagent /bye
-KEY_GRIP1=$(gpg-connect-agent -q 'keyinfo --list' /bye | awk '/KEYINFO/ { print $3 }' | head -1)
-KEY_GRIP2=$(gpg-connect-agent -q 'keyinfo --list' /bye | awk '/KEYINFO/ { print $3 }' | tail -1)
-/usr/libexec/gpg-preset-passphrase -c $KEY_GRIP1 <<< openshift
-/usr/libexec/gpg-preset-passphrase -c $KEY_GRIP2 <<< openshift
 git config --global gpg.program ~/.gpg
