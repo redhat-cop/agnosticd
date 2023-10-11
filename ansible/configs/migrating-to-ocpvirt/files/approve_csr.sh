f71a95bf040598b7a1a560831b969b9822505ae3
Alberto Gonzalez Rodriguez <alberto.gonzalez@redhat.com>
2023-10-11 08:28:29 +0100

    [migrating-to-ocpvirt Update approve_csr.sh

--- ansible/configs/migrating-to-ocpvirt/files/approve_csr.sh
+++ ansible/configs/migrating-to-ocpvirt/files/approve_csr.sh
@@ -1,4 +1,11 @@
 #!/bin/sh -x
+oc get csr 2>error.log
+ERRORS=$(wc -l < error.log)
+while [ $ERRORS -gt 0 ]; do
+  oc get csr 2>error.log
+  ERRORS=$(wc -l < error.log)
+done
+
 oc get csr|grep Pending|awk '{print $1}'|xargs -i oc adm certificate approve {}
 sleep 120
 READY=$((oc get nodes || echo NotReady) |grep -c " Ready ")
