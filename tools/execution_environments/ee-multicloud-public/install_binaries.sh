f9d27c26e19ce5bc4b99b2ea876753979566e692
Judd Maltin <judd@newgoliath.com>
2023-10-23 19:07:34 -0400

    add helm install per https://helm.sh/docs/intro/install/\#from-script

--- tools/execution_environments/ee-multicloud-public/install_binaries.sh
+++ tools/execution_environments/ee-multicloud-public/install_binaries.sh
@@ -33,3 +33,9 @@ unzip awscliv2.zip
 
 rm awscliv2.zip
 rm -rf aws
+
+# helm
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
