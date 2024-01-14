5ccdb7e873ab6eec614369183a84840e27ec2492
Alberto Gonzalez <josegonz@redhat.com>
2024-01-14 19:20:59 +0100

    Patch to accept wildcards

--- ansible/configs/hcp-ocpv/files/replace_certs.sh
+++ ansible/configs/hcp-ocpv/files/replace_certs.sh
@@ -5,4 +5,5 @@ oc create secret tls router-certs-new --cert=fullchain.pem --key=privkey.pem -n 
 oc patch ingresscontroller default -n openshift-ingress-operator --type=merge --patch='{"spec": { "defaultCertificate": { "name": "router-certs-new" }}}'
 oc create secret tls api-certs-new --cert=fullchain.pem --key=privkey.pem -n openshift-config
 oc patch apiserver cluster --type merge --patch="{\"spec\": {\"servingCerts\": {\"namedCertificates\": [ { \"names\": [  \"$LE_API\"  ], \"servingCertificate\": {\"name\": \"api-certs-new\" }}]}}}"
+oc patch ingresscontroller -n openshift-ingress-operator default --type=json -p '[{ "op": "add", "path": "/spec/routeAdmission", "value": {wildcardPolicy: "WildcardsAllowed"}}]'
 oc delete pod --all -n openshift-ingress --force
