daa649ee907b56aa9c650812f347a001dd97298d
Alberto Gonzalez Rodriguez <alberto.gonzalez@redhat.com>
2024-02-11 14:15:31 +0100

    Update configure_ssl.sh

--- ansible/configs/citrixvdi-ocpvirt/files/configure_ssl.sh
+++ ansible/configs/citrixvdi-ocpvirt/files/configure_ssl.sh
@@ -1,26 +1,26 @@
 cd /root/
 oc create configmap custom-ca \
      --from-file=ca-bundle.crt=chain1.pem \
-     -n openshift-config
+     -n openshift-config || true
 
 oc create secret tls letsencrypt \
      --cert=cert1.pem \
      --key=privkey1.pem \
-     -n openshift-ingress
+     -n openshift-ingress || true
 
 oc patch proxy/cluster \
   --type=merge --patch='{"spec":{"trustedCA":{"name":"custom-ca"}}}' \
-  -n openshift-config
+  -n openshift-config || true
 
 oc patch ingresscontroller.operator default \
      --type=merge -p \
      '{"spec":{"defaultCertificate": {"name": "letsencrypt"}}}' \
-     -n openshift-ingress-operator
+     -n openshift-ingress-operator || true
 
 oc create namespace openshift-cnv || true
 
 oc create configmap custom-ca \
      --from-file=ca-bundle.crt=chain1.pem \
-     -n openshift-cnv
+     -n openshift-cnv || true
 
 
