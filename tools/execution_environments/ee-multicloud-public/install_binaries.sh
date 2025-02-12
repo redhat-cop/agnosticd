756a4e0fe9876d7dc6133160b38643b1bf031417
Guillaume Coré <gucore@redhat.com>
2025-02-12 23:41:25 +0100

    Revert "Add multi-platform support for execution environment (#8951)" (#9040)
    This reverts commit 914b18dc02ed22839b6f7f0e8f316d4340e0c5bd.

--- tools/execution_environments/ee-multicloud-public/install_binaries.sh
+++ tools/execution_environments/ee-multicloud-public/install_binaries.sh
@@ -3,32 +3,19 @@ set -ue
 
 cd /tmp
 
-# initArch discovers the architecture for this system.
-ARCH=$(uname -m)
-case $ARCH in
-    armv5*) ARCH="armv5";;
-    armv6*) ARCH="armv6";;
-    armv7*) ARCH="arm";;
-    aarch64) ARCH="arm64";;
-    x86) ARCH="386";;
-    x86_64) ARCH="amd64";;
-    i686) ARCH="386";;
-    i386) ARCH="386";;
-esac
-
 # OC
-# Install rhel8 version of oc
-# https://access.redhat.com/solutions/7077895
+
 version=stable
-tarball=openshift-client-linux-${ARCH}-rhel8.tar.gz
-url="https://mirror.openshift.com/pub/openshift-v4/${ARCH}/clients/ocp/${version}/${tarball}"
+arch=x86_64
+tarball=openshift-client-linux.tar.gz
+url="https://mirror.openshift.com/pub/openshift-v4/${arch}/clients/ocp/${version}/${tarball}"
 curl -s -L "${url}" -o ${tarball}
 tar xzf ${tarball}
 install -t /usr/bin oc kubectl
 rm ${tarball}
 
 # Bitwarden
-# DISCLAIMER: BW doesn't support ARM64 yet, so this is just a placeholder
+
 url="https://vault.bitwarden.com/download/?app=cli&platform=linux"
 curl -s -L "${url}" -o bw.zip
 unzip bw.zip
@@ -37,8 +24,9 @@ rm bw bw.zip
 
 
 # AWS CLI
+
 aws_version=2.4.23
-curl -s -L "https://awscli.amazonaws.com/awscli-exe-linux-$(uname -m)-${aws_version}.zip" \
+curl -s -L "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-${aws_version}.zip" \
     -o "awscliv2.zip"
 unzip awscliv2.zip
 ./aws/install
